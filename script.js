3ec683f031e8be4e76d78eb56a1e68fc4d14bd0a
dannyarena <arenadanny71@gmail.com>
2025-04-24 16:05:17 +0200

    Implement email generation functionality in script.js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const emailList = document.getElementById('email-list');
 const button = document.getElementById('genera-email');
+
+function generaEmail() {
+    emailList.innerHTML = ''; // svuoto la lista
+    for (let i = 0; i < 10; i++) {
+        fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+          .then(response => response.json())
