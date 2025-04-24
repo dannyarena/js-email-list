e4f99a3d29519dd7749f13d98d82aaa56cb085c8
dannyarena <arenadanny71@gmail.com>
2025-04-24 16:06:54 +0200

    Refactor email generation function for improved readability and structure

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+
 const emailList = document.getElementById('email-list');
 const button = document.getElementById('genera-email');
 
 function generaEmail() {
-    emailList.innerHTML = ''; // svuoto la lista
-    for (let i = 0; i < 10; i++) {
-        fetch('https://flynn.boolean.careers/exercises/api/random/mail')
-          .then(response => response.json())
+  emailList.innerHTML = ''; // svuoto la lista
+  for (let i = 0; i < 10; i++) {
+    fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then(response => response.json())
+      .then(data => {
+        const li = document.createElement('li');
+        li.textContent = data.response;
+        emailList.appendChild(li);
+      });
+  }
+}
+
+button.addEventListener('click', generaEmail);
+
+// Genera subito 10 email quando carichi la pagina
+generaEmail();
